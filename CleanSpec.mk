821f65f8733279b6d17b80ab89e2ab06b67d9063
dadi11 <dadi.sikorski@gmail.com>
2017-03-04 21:44:13 +0100

    XenonApps: Amaze -> MiXplorer exchange
    6.12.9.BETA

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -14,8 +14,8 @@
 #
 
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Adaway)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Amaze)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/KernelAdiutor)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/MiXplorer)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/NovaLauncher)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/substratum)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SuperSU)
