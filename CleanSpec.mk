04eafbe03db2241c7c18dc970bf25d9496bcca94
dadi11 <dadi.sikorski@gmail.com>
2017-10-24 01:31:14 +0200

    XenonApps: remove magisk
    Change-Id: I688e7c654cce5728b1040c78d1e5129f580cf1d7

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -13,7 +13,6 @@
 # limitations under the License.
 #
 
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Magisk)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/NovaLauncher)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/PixelTheme)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/StockTheme)
